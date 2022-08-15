1071ec052769a703ca25f33a45b5a017cbb3ec26
_nkalways <kishanko14@gmail.com>
2022-08-15 21:49:01 +0530

    added spread sheet

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 
 function validate(){
-var username = document.getElementById("usn").value;
-var password = document.getElementById("password").value;
-if ( usn == "USN" && password == "123"){
-alert ("Login successfully");
-window.location.href="./user.html"
-document.login.submit();
-return false;
+var username = document.getElementsByClassName('usnfill').value;
+var password = document.getElementsByClassName('passfill').value;
+if ( username == "1111" && password == "1111"){
+alert ('Login successfully');
+window.location.href = "file:///D:/vscode/student_data_form/user.html";
+
 }
 else{
     alert('Login Failed');
